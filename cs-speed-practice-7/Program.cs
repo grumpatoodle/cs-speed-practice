a06acf06a260463f047e7af1d6806eab72d72b87
Hebah <hebahsaleh@gmail.com>
2019-05-22 10:16:00 -0500

    feat(7)
    Palindrome

---
+++ cs-speed-practice-7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace cs_speed_practice_7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var realProgram = new MyActualProgram();
+            realProgram.StartHere();
+        }
+    }
+
+    // Write a formula to determine whether a word is a Palindrome or not.
+
+    public class MyActualProgram
+    {
+        public void StartHere()
+        {
+            var thisVariableShouldNeverBeUsed = "PUT A BREAK POINT ON THIS LINE AND DEBUG EVERY THING YOU EVER DO";
+
+            Console.Write("Please enter a word to check if it is a Palindrome: ");
+            var input = Console.ReadLine();
+
+            char[] inputChar = input.ToCharArray();
+
+            Array.Reverse(inputChar);
+            var reverse = new string(inputChar);
+
+            Console.WriteLine(reverse);
+
+            bool isPalindrome = input.Equals(reverse);
+
+            if (isPalindrome == true)
+            {
+                Console.WriteLine("This is a Palindrome!");
+            }
+            else
+            {
+                Console.WriteLine("This is not a Palindrome.");
+            }
+        }
+    }
+}
