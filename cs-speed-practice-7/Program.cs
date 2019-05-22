db8033392e8dd134aeb9086846359218ad37fb6b
Hebah <hebahsaleh@gmail.com>
2019-05-22 13:54:36 -0500

    WIP(8)
    practicing with arrays

--- cs-speed-practice-7/Program.cs
+++ cs-speed-practice-7/Program.cs
@@ -13,6 +13,7 @@ namespace cs_speed_practice_7
     }
 
     // Write a formula to determine whether a word is a Palindrome or not.
+    // Make it a loop
 
     public class MyActualProgram
     {
@@ -20,25 +21,33 @@ namespace cs_speed_practice_7
         {
             var thisVariableShouldNeverBeUsed = "PUT A BREAK POINT ON THIS LINE AND DEBUG EVERY THING YOU EVER DO";
 
-            Console.Write("Please enter a word to check if it is a Palindrome: ");
-            var input = Console.ReadLine();
-
-            char[] inputChar = input.ToCharArray();
-
-            Array.Reverse(inputChar);
-            var reverse = new string(inputChar);
-
-            Console.WriteLine(reverse);
-
-            bool isPalindrome = input.Equals(reverse);
-
-            if (isPalindrome == true)
+            while (true)
             {
-                Console.WriteLine("This is a Palindrome!");
-            }
-            else
-            {
-                Console.WriteLine("This is not a Palindrome.");
+                Console.Write("Please enter a word to check if it is a Palindrome: ");
+                var input = Console.ReadLine();
+
+                if (input == "exit")
+                {
+                    break;
+                }
+
+                char[] inputChar = input.ToCharArray();
+
+                Array.Reverse(inputChar);
+                var reverse = new string(inputChar);
+
+                Console.WriteLine(reverse);
+
+                bool isPalindrome = input.Equals(reverse);
+
+                if (isPalindrome == true)
+                {
+                    Console.WriteLine("This is a Palindrome!");
+                }
+                else
+                {
+                    Console.WriteLine("This is not a Palindrome.");
+                }
             }
         }
     }
---
+++ cs-speed-practice-8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace cs_speed_practice_8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var realProgram = new MyActualProgram();
+            realProgram.StartHere();
+        }
+    }
+
+    // Write a program to store elements in an array and print it. 
+
+    public class MyActualProgram
+    {
+        public void StartHere()
+        {
+            var thisVariableShouldNeverBeUsed = "PUT A BREAK POINT ON THIS LINE AND DEBUG EVERY THING YOU EVER DO";
+
+            int[] array = new[] { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            for (var i = 1; i < array.GetLength(0); i++)
+            {
+                i = array[i];
+                Console.Write(array[i] + " ");
+            }
+        }
+    }
+}
