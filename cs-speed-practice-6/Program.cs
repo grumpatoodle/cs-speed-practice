e00696e731cde3247866b646e6cdfbe49dbe10cc
Hebah <hebahsaleh@gmail.com>
2019-05-22 09:20:12 -0500

    feat(6)

---
+++ cs-speed-practice-6/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace cs_speed_practice_6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var realProgram = new MyActualProgram();
+            realProgram.StartHere();
+        }
+    }
+
+    // Write a C# Sharp program to accept a coordinate point in an XY coordinate system and determine in which quadrant the coordinate point lies.
+    // Test Data :
+    // Input the value for X coordinate : 7 
+    // Input the value for Y coordinate : 9 
+    // Expected Output :
+    // The coordinate point (7,9) lies in the First quadrant.
+
+    public class MyActualProgram
+    {
+        public void StartHere()
+        {
+            var thisVariableShouldNeverBeUsed = "PUT A BREAK POINT ON THIS LINE AND DEBUG EVERY THING YOU EVER DO";
+
+            Console.Write("Please enter a value for x: ");
+            var x = int.Parse(Console.ReadLine());
+            Console.Write("Please enter a value for y: ");
+            var y = int.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Your coordinate is in Quadrant I.");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Your coordinate is in Quardrant II.");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Your coordinate is in Quadrant III.");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Your coordinate is in Quadrant IV.");
+            }
+            else if (x == 0 && y == 00)
+            {
+                Console.WriteLine("Your coordinate is at the origin.");
+            }
+        }
+    }
+}
